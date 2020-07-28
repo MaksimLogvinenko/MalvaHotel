bda3be3e78ba7b1dd6ce77e3829d6a06b8d2294c
Vitaliy Protsyk <eqellez@gmail.com>
2020-07-28 23:11:05 +0300

    Changed all

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
-$('.navbar-burger').click(function(e){
-    e.preventDefault();
-    $('.navbar-menu').toggleClass('menu_active');
+$(".navbar__menu-btn").click(function (e) {
+  e.preventDefault();
+  $(".navbar-menu").toggleClass("menu_active");
 });
-$('.navbar-menu_close').click(function(e){
-    e.preventDefault();
-    $('.navbar-menu').removeClass('menu_active');
+$(".navbar-menu_close").click(function (e) {
+  e.preventDefault();
+  $(".navbar-menu").removeClass("menu_active");
 });
-const nav = $('.navbar-wrap');
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 50) {
-            $('.navbar-logo').remove('img');
-			nav.addClass("navbar-wrap_active");
-		} else {
-			nav.removeClass("navbar-wrap_active");
-		}
-	});
-$('.navbar-language').click(function(e){
-	e.preventDefault();
-	$('.navbar-language_menu').slideToggle();
-	let slideArrow = $('.navbar-language');
-	slideArrow.toggleClass('navbar-language_active');
+
+const navbar = $(".navbar");
+const header = $(".header")
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+		header.addClass("header--pinned");
+    navbar.addClass("navbar--pinned");
+  } else {
+		header.removeClass("header--pinned");
+    navbar.removeClass("navbar--pinned");
+  }
 });
-$('.slider-menu').slick({
-	infinite: true,
-	slidesToShow: 4,
-	variableWidth: true,
-	slidesToScroll: 2,
-	dots: true,
-	arrows: true
-  });
+$(".navbar-language").click(function (e) {
+  e.preventDefault();
+  $(".navbar-language_menu").slideToggle();
+  let slideArrow = $(".navbar-language");
+  slideArrow.toggleClass("navbar-language_active");
+});
+$(".slider-menu").slick({
+  infinite: true,
+  slidesToShow: 4,
+  variableWidth: true,
+  slidesToScroll: 2,
+  dots: true,
+  arrows: true,
+});
